2c4c1610978a05df69edf33c76df45110fc572c0
archmoj <mojtaba@plot.ly>
2020-11-29 22:03:17 -0500

    add option to start decoding after start index

--- test/base64-arraybuffer_test.js
+++ test/base64-arraybuffer_test.js
@@ -52,5 +52,7 @@ describe('decode', () => {
   it('decode "Man"', () => ok(testArrayBuffers(decode("TWFu"), stringArrayBuffer("Man"))));
   it('decode "Hello world"', () => ok(testArrayBuffers(decode("SGVsbG8gd29ybGQ="), stringArrayBuffer("Hello world"))));
   it('decode "Hello worlds!"', () => ok(testArrayBuffers(decode("SGVsbG8gd29ybGRzIQ=="), stringArrayBuffer("Hello worlds!"))));
+  it('decode "Hello world" - discard start < 0', () => ok(testArrayBuffers(decode("SGVsbG8gd29ybGQ=", -1), stringArrayBuffer("Hello world"))));
+  it('decode "Hello world" after header when start < length', () => ok(testArrayBuffers(decode("data:image/bmp;base64,SGVsbG8gd29ybGQ=", 22), stringArrayBuffer("Hello world"))));
   it('decode all binary characters', () => ok(testArrayBuffers(decode("AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5fYGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6PkJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq+wsbKztLW2t7i5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+/w=="), rangeArrayBuffer())));
 });
