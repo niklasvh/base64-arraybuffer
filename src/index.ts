03a24678e6835a42e697059ba4f3b9ecac819d80
Clément Nerma <clement.nerma@gmail.com>
2022-03-18 12:59:03 +0100

    Improve code quality
    Essentially:
    - Use constants where possible
    - Declare variables and constants just before they are used to avoid mixing them

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,67 @@
-const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+/**
+ * Adapted from https://github.com/niklasvh/base64-arraybuffer
+ * Will be remade using a WebAssembly module later on for performance
+ */
 
-// Use a lookup table to find the index.
-const lookup = typeof Uint8Array === 'undefined' ? [] : new Uint8Array(256);
-for (let i = 0; i < chars.length; i++) {
-    lookup[chars.charCodeAt(i)] = i;
+export function encode(arraybuffer: ArrayBuffer): string {
+  const bytes = new Uint8Array(arraybuffer)
+  const len = bytes.length
+
+  const base64: string[] = []
+
+  for (let i = 0; i < len; i += 3) {
+    base64.push(CHARS[bytes[i] >> 2])
+    base64.push(CHARS[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)])
+    base64.push(CHARS[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)])
+    base64.push(CHARS[bytes[i + 2] & 63])
+  }
+
+  const str = base64.join('')
+
+  if (len % 3 === 2) {
+    return str.substring(0, str.length - 1) + '='
+  } else if (len % 3 === 1) {
+    return str.substring(0, str.length - 2) + '=='
+  } else {
+    return str
+  }
 }
 
-export const encode = (arraybuffer: ArrayBuffer): string => {
-    let bytes = new Uint8Array(arraybuffer),
-        i,
-        len = bytes.length,
-        base64 = '';
+export function decode(base64: string): ArrayBuffer {
+  let bufferLength = base64.length * 0.75
+  const len = base64.length
 
-    for (i = 0; i < len; i += 3) {
-        base64 += chars[bytes[i] >> 2];
-        base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
-        base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
-        base64 += chars[bytes[i + 2] & 63];
+  if (base64.substr(-1) === '=') {
+    bufferLength -= 1
+
+    if (base64.substr(-2, 1) === '=') {
+      bufferLength -= 1
     }
+  }
 
-    if (len % 3 === 2) {
-        base64 = base64.substring(0, base64.length - 1) + '=';
-    } else if (len % 3 === 1) {
-        base64 = base64.substring(0, base64.length - 2) + '==';
-    }
+  const decoded = new ArrayBuffer(bufferLength)
+  const bytes = new Uint8Array(decoded)
 
-    return base64;
-};
+  let p = 0
 
-export const decode = (base64: string): ArrayBuffer => {
-    let bufferLength = base64.length * 0.75,
-        len = base64.length,
-        i,
-        p = 0,
-        encoded1,
-        encoded2,
-        encoded3,
-        encoded4;
+  for (let i = 0; i < len; i += 4) {
+    const encoded1 = LOOKUP[base64.charCodeAt(i)]
+    const encoded2 = LOOKUP[base64.charCodeAt(i + 1)]
+    const encoded3 = LOOKUP[base64.charCodeAt(i + 2)]
+    const encoded4 = LOOKUP[base64.charCodeAt(i + 3)]
 
-    if (base64[base64.length - 1] === '=') {
-        bufferLength--;
-        if (base64[base64.length - 2] === '=') {
-            bufferLength--;
-        }
-    }
+    bytes[p++] = (encoded1 << 2) | (encoded2 >> 4)
+    bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2)
+    bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63)
+  }
 
-    const arraybuffer = new ArrayBuffer(bufferLength),
-        bytes = new Uint8Array(arraybuffer);
+  return decoded
+}
 
-    for (i = 0; i < len; i += 4) {
-        encoded1 = lookup[base64.charCodeAt(i)];
-        encoded2 = lookup[base64.charCodeAt(i + 1)];
-        encoded3 = lookup[base64.charCodeAt(i + 2)];
-        encoded4 = lookup[base64.charCodeAt(i + 3)];
+const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
-        bytes[p++] = (encoded1 << 2) | (encoded2 >> 4);
-        bytes[p++] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
-        bytes[p++] = ((encoded3 & 3) << 6) | (encoded4 & 63);
-    }
+const LOOKUP = typeof Uint8Array === 'undefined' ? [] : new Uint8Array(256)
 
-    return arraybuffer;
-};
+for (let i = 0; i < CHARS.length; i++) {
+  LOOKUP[CHARS.charCodeAt(i)] = i
+}
